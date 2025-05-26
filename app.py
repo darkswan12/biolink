d58572ba08e9da3bbf1538ebc1f2c37d2650bd51
darmawan suhara <dsuharaaa@gmail.com>
2025-05-27 02:50:02 +0700

    nambah link baru

--- app.py
+++ app.py
@@ -12,6 +12,7 @@ def home():
             {'name': 'GitHub', 'url': 'https://github.com/darkswan12', 'icon': 'github', 'prefix': 'fab'},
             {'name': 'LinkedIn', 'url': 'https://www.linkedin.com/in/darmawan-suhara-a9426422b/', 'icon': 'linkedin', 'prefix': 'fab'},
             {'name': 'Instagram', 'url': 'https://instagram.com/darkswan.12', 'icon': 'instagram', 'prefix': 'fab'},
+            {'name': 'Discord', 'url': 'https://discord.com/users/275947812439719936', 'icon': 'discord', 'prefix': 'fab'},
             {'name': 'Portfolio', 'url': 'https://darkswan12.pythonanywhere.com/', 'icon': 'address-card', 'prefix': 'fas'},
             {'name': 'Carousel Crop Tools', 'url': 'https://darskwan10.pythonanywhere.com/', 'icon': 'cut', 'prefix': 'fas'},
             {'name': 'Tiktok', 'url': 'https://www.tiktok.com/@darkswan.12', 'icon': 'tiktok', 'prefix': 'fab'},
